8e0e858d19e351812b4c75a9f1712fe61551b930
Evenlaxxus <r.ewiak1404@wp.pl>
2019-10-14 23:39:42 +0200

    lab2 zad1-5

--- Lab2/src/Firma.java
+++ Lab2/src/Firma.java
@@ -3,29 +3,30 @@ import java.util.LinkedList;
 import java.lang.Iterable;
 
 public class Firma implements Iterable{
-    private LinkedList<Pracownik> pracownik;
+    private LinkedList<Pracownik> pracownicy;
 
     Firma(){
-        this.pracownik=new LinkedList<Pracownik>();
+        this.pracownicy=new LinkedList<Pracownik>();
     }
     public void dodaj_pracownika(Pracownik pracownik){
-        this.pracownik.add(pracownik);
+        this.pracownicy.add(pracownik);
     }
 
     public int liczba_pracownikow(){
-        return this.pracownik.size();
+        return this.pracownicy.size();
     }
 
     public void lista_pracownikow() {
         for(int i=0;i<this.liczba_pracownikow();i++){
-            System.out.println(this.pracownik.get(i).toString());
+            System.out.println(this.pracownicy.get(i).toString());
         }
     }
 
 
     @Override
     public Iterator<Pracownik> iterator() {
-        return this.pracownik.iterator();
+        return this.pracownicy.iterator();
 
     }
+    
 }
--- Lab2/src/Stanowisko.java
+++ Lab2/src/Stanowisko.java
@@ -3,8 +3,25 @@ public enum Stanowisko{
         Kierownik("Kierownik",1,"opis2"),
         Pracownik("Pracownik",1,"opis3");
 
-    Stanowisko(final String nazwa, final int poziom, final String opis){
+    private final String nazwa;
+    private final int poziom;
+    private final String opis;
+    Stanowisko(String nazwa, int poziom, String opis){
+        this.nazwa=nazwa;
+        this.poziom=poziom;
+        this.opis=opis;
     }
 
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public int getPoziom() {
+        return poziom;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
 }
 
