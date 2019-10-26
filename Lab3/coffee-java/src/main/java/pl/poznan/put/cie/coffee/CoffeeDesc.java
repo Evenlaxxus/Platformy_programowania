5c79058314914bbf611d2e6c6f8da9b506144275
Evenlaxxus <r.ewiak1404@wp.pl>
2019-10-26 14:48:00 +0200

    zad1-2

---
+++ Lab3/coffee-java/src/main/java/pl/poznan/put/cie/coffee/CoffeeDesc.java
@@ -0,0 +1,48 @@
+package pl.poznan.put.cie.coffee;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+class CreateCoffeeDesc{
+    private static SessionFactory sessionFactory;
+
+    public static void main( String[ ] args ) {
+        try {
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+
+        CreateCoffeeDesc createCoffe = new CreateCoffeeDesc();
+        createCoffe.addCoffeDesc("Colombian", "Dobra kawucha");
+
+    }
+
+    /* Method to CREATE an employee in the database */
+    public String addCoffeDesc(String name, String description){
+        Session session = sessionFactory.openSession();
+        Transaction tx = null;
+        String coffeDescriptionID = null;
+
+        try {
+            tx = session.beginTransaction();
+            CoffeeDescriptionsEntity coffeeDescription = new CoffeeDescriptionsEntity();
+            coffeeDescription.setCofName(name);
+            coffeeDescription.setCofDesc(description);
+
+            coffeDescriptionID = (String) session.save(coffeeDescription);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx!=null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return coffeDescriptionID;
+    }
+
+}
